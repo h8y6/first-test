daf814723ae44a07bff37b52c323d61131e76ee9
h8y6 <h8y6mm@163.com>
2019-10-09 15:02:47 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # first-test
-试试看
+gulp自动化构建
+功能：1、压缩    可压缩html、img、css、js    gulp-htmlmin、gulp-imagemin、imagemin-pngquant、gulp-minify-css、gulp-cssnano、gulp-uglify    gulp-concat（js合并）
+      2、编译    编译less、sass等等    gulp-less
+	  3、自动化  实时刷新
+
+项目构建前置准备：
+	  1、安装node、检查node版本   node -v
+	  2、安装gulp（全局安装、已安装可忽略）  npm install gulp –g
+	  3、在当前项目下进行环境编译    npm init
---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require("gulp");
+gulp.task("default", function(a) {
+  console.log("hello world");
+  a();
+});
+
+
+gulp.task("copyHtml",function(){
+    gulp.src("src/*.html") //当前目录下所有的html文件
+        .pipe(gulp.dest("dist")) //文件的去处
+})//dist是通过 gulp copyHtml生成的文件夹
+
+
+var gulp = require("gulp");
+var uglify = require("gulp-uglify");  //js压缩
+var pump = require("pump");
+
+
+var imagemin = require('gulp-imagemin');   图片压缩
+var uglify = require("gulp-uglify");  js压缩
+var less = require('gulp-less');      less
+var cleanCSS = require('gulp-clean-css');  css压缩
+
+gulp.task("compress", function(cb) {
+  pump([gulp.src("src/js/*.js"), uglify(), gulp.dest("dist/js")], cb);
+});
+
+var gulp = require("gulp");
+var cssnano = require("gulp-cssnano");
+
+// 定义一个处理css文件改动的任务
+gulp.task("css", function(cb) {
+  gulp
+    .src("src/css/*.css")
+    .pipe(cssnano())
+    .pipe(
+      gulp.dest("dist/css"),
+      cb
+    );
+});
+
+var gulp = require("gulp");
+var imagemin = require("gulp-imagemin");
+var pngquant = require("imagemin-pngquant"); //png图片压缩插件
+
+gulp.task("default", function() {
+  return gulp
+    .src("src/images/*")
+    .pipe(
+      imagemin({
+        progressive: true,
+        use: [pngquant()] //使用pngquant来压缩png图片
+      })
+    )
+    .pipe(gulp.dest("dist/images"));
+});
+
+gulp.task("minify", function() {
+  return gulp
+    .src("src/pages*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("dist/pages"));
+});
