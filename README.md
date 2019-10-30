b26cf769edaec240a30dc1a581fa80b15e5c47f7
h8y6 <h8y6mm@163.com>
2019-10-30 11:55:59 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,10 +1,68 @@
 # first-test
-gulp自动化构建
-功能：1、压缩    可压缩html、img、css、js    gulp-htmlmin、gulp-imagemin、imagemin-pngquant、gulp-minify-css、gulp-cssnano、gulp-uglify    gulp-concat（js合并）
-      2、编译    编译less、sass等等    gulp-less
-	  3、自动化  实时刷新
+
+
+因为没有经验，又是自己动手，所以特别乱，定个文档来捋一捋
+所需要求：
+	1、压缩js、css、image、html
+	2、实时自动编译刷新
+	3、加 md5（补充）
+
+
+
+gulp 自动化构建
+功能：1、压缩 可压缩 html、img、css、js gulp-htmlmin、gulp-imagemin、imagemin-pngquant、gulp-minify-css、gulp-cssnano、gulp-uglify gulp-concat（js 合并）
+2、编译 编译 less、sass 等等 gulp-less
+3、自动化 实时刷新
 
 项目构建前置准备：
-	  1、安装node、检查node版本   node -v
-	  2、安装gulp（全局安装、已安装可忽略）  npm install gulp –g
-	  3、在当前项目下进行环境编译    npm init
+1、安装 node、检查 node 版本 node -v
+2、安装 gulp（全局安装、已安装可忽略） npm install gulp –g
+3、在当前项目下进行环境编译 npm init
+
+gulp-clean-css(压缩 css 文件)
+gulp-uglify(压缩 js 文件)
+gulp-concat（合并文件 js、css）
+gulp-rename(文件重命名)
+gulp-livereload（实时自动编译刷新）
+gulp-html-minify (压缩 html/jsp)
+gulp-rev （加 md5 的工具）
+gulp-rev-collector (根据 rev 生成的 manifest.json 文件中的映射, 去替换文件名称, 也可以替换路径)
+
+常用插件有如下：
+gulp-autoprefixer 根据设置浏览器版本自动处理浏览器前缀   不兼容display:box;  支持flex
+gulp-clean：用于清理;
+gulp-notify：用于打印消息文本;
+gulp-rename：用于修改名字;
+gulp-concat：用于合并文件;
+gulp-zip：用于生成一个 zip 压缩包;
+gulp-minify-css：用于压缩 css;
+gulp-autoprefixer：用于给 css 添加前缀;
+gulp-imagemin：用于给图片进行优化;
+gulp-uglify：用于压缩 js;
+amd-optimize：用于 amd 模块引用编译；
+gulp-import-css：如果 css 文件是通过 import 导入的可以使用此插件进行合并优化;
+gulp-rev-replace：用于替换;
+gulp-useref：引入使用 build 标记，进行替换;
+gulp-rev：生成 md5 文件名;
+gulp-filter：对文件进行过滤;
+gulp-header：压缩之后将注释写入到文件的头部
+gulp-if：进行逻辑判断
+gulp-size：获取文件大小
+gulp-less：编译 less 文件
+gulp-sass：编译 sass 文件
+gulp-file-include：对文件进行引入
+gulp-sourcemaps：生成 map 文件
+gulp-livereload：自动刷新
+gulp-clean-css：css 压缩
+browser-sync：启动 server 并启动热更新
+gulp-plumber : 监测工作流，报错，防止遇到错误时直接退出 gulp
+gulp-rev : 文件名添加版本号
+gulp-css-spritesmith：根据 css 文件自动生成雪碧图
+gulp-rev-dxb、gulp-rev-collector-dxb  给文件添加版号
+
+
+如果要查找 gulp 插件，一般有两个地方：
+
+官方插件库
+npm 仓库
+安装完所需插件后（-s 安装），package.json 文件内容自动改变
--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,138 @@
-var gulp = require("gulp");
-gulp.task("default", function(a) {
-  console.log("hello world");
-  a();
+// 加md5  前需要先清缓存
+var gulp = require("gulp"), // gulp基础库
+  rename = require("gulp-rename"), // 文件重命名
+  uglify = require("gulp-uglify"), // js压缩
+  htmlmin = require("gulp-htmlmin"), // html压缩
+  cleancss = require("gulp-clean-css"), // css压缩
+  imagemin = require("gulp-imagemin"), // 图片压缩
+  watch = require('gulp-watch');//文件监听
+
+
+
+//图片压缩
+gulp.task('imagemin', done => {
+    gulp.src("src/images/*") 
+	.pipe(imagemin())
+    .pipe(gulp.dest("dist/images"))  //放入到dist目录下面的images文件
+	done();
 });
 
 
-gulp.task("copyHtml",function(){
-    gulp.src("src/*.html") //当前目录下所有的html文件
-        .pipe(gulp.dest("dist")) //文件的去处
-})//dist是通过 gulp copyHtml生成的文件夹
 
-
-var gulp = require("gulp");
-var uglify = require("gulp-uglify");  //js压缩
-var pump = require("pump");
-
-
-var imagemin = require('gulp-imagemin');   图片压缩
-var uglify = require("gulp-uglify");  js压缩
-var less = require('gulp-less');      less
-var cleanCSS = require('gulp-clean-css');  css压缩
-
-gulp.task("compress", function(cb) {
-  pump([gulp.src("src/js/*.js"), uglify(), gulp.dest("dist/js")], cb);
+// js处理
+gulp.task('minifyjs', done => {
+    gulp.src("./src/js/*.js") 
+    // .pipe(rename({ suffix: ".min" })) //重命名
+    .pipe(uglify({mangle: false//是否修改变量名
+	})) //压缩
+    .pipe(gulp.dest("./dist/js"))  //放入到dist目录下面的images文件
+	 done();
 });
 
-var gulp = require("gulp");
-var cssnano = require("gulp-cssnano");
+// css处理
+gulp.task('cleancss', done => {
+    gulp.src("./src/css/**/*.css") 
+	.pipe(cleancss({compatibility: 'ie8'}))
+    .pipe(gulp.dest("./dist/css"))  //放入到dist目录下面的css文件
+	 done();
+})	
 
-// 定义一个处理css文件改动的任务
-gulp.task("css", function(cb) {
-  gulp
-    .src("src/css/*.css")
-    .pipe(cssnano())
-    .pipe(
-      gulp.dest("dist/css"),
-      cb
-    );
+
+gulp.task('copyHtml', done => {
+    gulp.src("src/*.html") 
+	.pipe(htmlmin({ 
+				removeComments: true,       // 清除HTML注释
+				collapseWhitespace: true,   // 压缩HTML
+				minifyJS: true,             // 压缩页面JS
+				minifyCSS: true             // 压缩页面CSS
+	}))
+    .pipe(gulp.dest("dist"))  //把src目录中所有html格式的文件全部合并到dist目录中
+    done();
 });
 
-var gulp = require("gulp");
-var imagemin = require("gulp-imagemin");
-var pngquant = require("imagemin-pngquant"); //png图片压缩插件
+gulp.task('default', gulp.series("minifyjs","cleancss","imagemin","copyHtml"));
 
-gulp.task("default", function() {
-  return gulp
-    .src("src/images/*")
-    .pipe(
-      imagemin({
-        progressive: true,
-        use: [pngquant()] //使用pngquant来压缩png图片
-      })
-    )
-    .pipe(gulp.dest("dist/images"));
-});
 
-gulp.task("minify", function() {
-  return gulp
-    .src("src/pages*.html")
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest("dist/pages"));
-});
+
+
+// // 加md5  前需要先清缓存
+// var gulp = require("gulp"), // gulp基础库
+//   rename = require("gulp-rename"), // 文件重命名
+//   uglify = require("gulp-uglify"), // js压缩
+//   htmlmin = require("gulp-htmlmin"), // html压缩
+//   cleancss = require("gulp-clean-css"), // css压缩
+//   imagemin = require("gulp-imagemin"), // 图片压缩
+//   watch = require('gulp-watch'),//文件监听
+//   autoprefixer = require('gulp-autoprefixer'),
+//   rev = require('gulp-rev'),   //- 对css、js文件名加MD5后缀
+//   clean=require('gulp-clean'),//清理             
+//   revCollector = require('gulp-rev-collector');   //- 路径替换
+
+
+
+// //图片压缩
+// gulp.task('imagemin', done => {
+//     gulp.src("src/images/*") 
+// 	.pipe(imagemin())
+//     .pipe(gulp.dest("dist/images"))  //放入到dist目录下面的images文件
+// 	done();
+// });
+
+
+
+// // js处理
+// gulp.task('minifyjs', done => {
+//     gulp.src("./src/js/*.js") 
+//     .pipe(rename({ suffix: ".min" })) //重命名
+//     .pipe(uglify({mangle: false//是否修改变量名
+// 	})) //压缩
+//     .pipe(rev())             //- 文件名加MD5后缀
+//     .pipe(gulp.dest("./dist/js"))  //放入到dist目录下面的images文件
+//     .pipe(rev.manifest())                       //- 生成一个rev-manifest.json
+//     .pipe(gulp.dest('./rev/js'))                  //- 将rev-manifest.json保存到 rev-js 目录内
+// 	 done();
+// });
+
+// // css处理
+// gulp.task('cleancss', done => {
+//     gulp.src("./src/css/**/*.css") 
+// 	.pipe(cleancss({compatibility: 'ie8'}))
+//     .pipe(rev())             //- 文件名加MD5后缀
+//     .pipe(gulp.dest("./dist/css"))  //放入到dist目录下面的css文件
+//     .pipe(rev.manifest())                       //- 生成一个rev-manifest.json
+//     .pipe(gulp.dest('./rev/css'))                  //- 将rev-manifest.json保存到 rev-js 目录内
+// 	 done();
+// })	
+
+// //路径替换任务
+// gulp.task('rev', done =>{
+//     gulp.src(['./rev/*/*json','./src/*.html'])
+//         .pipe(revCollector())
+//         .pipe(gulp.dest('./dist'));                  //- 将rev-manifest.json保存到 dist 目录内
+// 	 done();
+// });
+// //清理文件
+// gulp.task('clean', done =>{
+//     gulp.src(['./dist'], {read: false})
+//         .pipe(clean());                  //- 将rev-manifest.json保存到 rev-js 目录内
+// 	 done();
+// });
+
+
+// gulp.task('copyHtml', done => {
+//     gulp.src(['rev/css/*.json','rev/js/*.json',"src/*.html"]) 
+// 	.pipe(revCollector({replaceReved: true}))
+// 	.pipe(htmlmin({ 
+// 				removeComments: true,       // 清除HTML注释
+// 				collapseWhitespace: true,   // 压缩HTML
+// 				minifyJS: true,             // 压缩页面JS
+// 				minifyCSS: true             // 压缩页面CSS
+// 	}))
+//     .pipe(gulp.dest("dist"))  //把src目录中所有html格式的文件全部合并到dist目录中
+//     done();
+// });
+
+// gulp.task('default', gulp.series("minifyjs","cleancss","imagemin","rev","copyHtml"));
+
+
+
